aab15b7d4bea8b5cef2884f8ea54989b55a5667c
Shree Ram IT Infotech <304928995+shreeramitinfotech@users.noreply.github.com>
2026-08-26 03:24:23 +0530

    Update 00_setup_env_native_chimera_lto.sh

--- ci/test/00_setup_env_native_chimera_lto.sh
+++ ci/test/00_setup_env_native_chimera_lto.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) The Bitcoin Core developers
+# Copyright (c) The Sriicoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit.
 
@@ -16,7 +16,7 @@ export PIP_PACKAGES="--break-system-packages pyzmq pycapnp"
 # NO_QT=1 because Qt needs various patches: https://github.com/chimera-linux/cports/tree/master/main/qt6-qtbase/patches
 export DEP_OPTS="build_CC=clang build_CXX=clang++ build_TAR=gtar AR=llvm-ar CC=clang CXX=clang++ NM=llvm-nm RANLIB=llvm-ranlib STRIP=llvm-strip NO_QT=1"
 export GOAL="install"
-export BITCOIN_CONFIG="\
+export SRIICOIN_CONFIG="\
  --preset=dev-mode \
  -DBUILD_GUI=OFF \
  -DREDUCE_EXPORTS=ON \
