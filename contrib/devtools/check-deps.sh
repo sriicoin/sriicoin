48879c9c8beba8465d7d266c10cf91752111d633
Shree Ram IT Infotech <304928995+shreeramitinfotech@users.noreply.github.com>
2026-08-26 03:20:35 +0530

    Update check-deps.sh

--- contrib/devtools/check-deps.sh
+++ contrib/devtools/check-deps.sh
@@ -5,13 +5,13 @@ set -Eeuo pipefail
 
 # Declare paths to libraries
 declare -A LIBS
-LIBS[cli]="libbitcoin_cli.a"
-LIBS[common]="libbitcoin_common.a"
-LIBS[consensus]="libbitcoin_consensus.a"
-LIBS[crypto]="libbitcoin_crypto.a"
-LIBS[node]="libbitcoin_node.a"
-LIBS[util]="libbitcoin_util.a"
-LIBS[wallet]="libbitcoin_wallet.a"
+LIBS[cli]="libsriicoin_cli.a"
+LIBS[common]="libsriicoin_common.a"
+LIBS[consensus]="libsriicoin_consensus.a"
+LIBS[crypto]="libsriicoin_crypto.a"
+LIBS[node]="libsriicoin_node.a"
+LIBS[util]="libsriicoin_util.a"
+LIBS[wallet]="libsriicoin_wallet.a"
 
 # Declare allowed dependencies "X Y" where X is allowed to depend on Y. This
 # list is taken from doc/design/libraries.md.
@@ -44,7 +44,7 @@ declare -A SUPPRESS
 # init/common.cpp file calls InitError and InitWarning from interface_ui which
 # is currently part of the node library. interface_ui should just be part of the
 # common library instead, and is moved in
-# https://github.com/bitcoin/bitcoin/issues/10102
+# https://github.com/sriicoin/sriicoin/issues/10102
 SUPPRESS["common.cpp.o interface_ui.cpp.o _Z11InitWarningRK13bilingual_str"]=1
 SUPPRESS["common.cpp.o interface_ui.cpp.o _Z9InitErrorRK13bilingual_str"]=1
 
